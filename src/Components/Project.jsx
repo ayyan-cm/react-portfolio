869070e73c3984c20ec5f7497afc37baeb450460
ayyan-cm <cayyanraj@gmai.com>
2024-08-02 00:06:45 +0530

    added carousel pointer in project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -27,7 +27,7 @@ export default function Project() {
   const centerStyle =
     "relative flex flex-col items-center justify-center w-40 h-40 md:w-52 md:h-52 lg:w-60 lg:h-60 rounded-lg my-5  text-white hover:scale-110 transition-transform duration-300 ease-in-out";
   const sideStyle =
-    "flex flex-col items-center justify-center w-20 h-40 md:w-42 md:h-52 lg:w-60 lg:h-60 rounded-lg my-5 scale-75 ";
+    "flex flex-col items-center justify-center w-20 h-40 md:w-42 md:h-52 lg:w-60 lg:h-60 rounded-lg my-5 scale-75 hover:cursor-pointer";
 
   return (
     <div className="flex flex-col text-center mt-5 border-t-2 border-t-zinc-800 p-5 m-7 md:m-12 lg:mx-20">
@@ -36,96 +36,114 @@ export default function Project() {
       </div>
 
       {/* Project Carousel */}
-      <div {...bind()} className="flex justify-center items-center touch-none">
-        <div className="hidden md:block">
-          <button onClick={prevProject} aria-label="Previous Project">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={sideStyle}
-          style={{
-            backgroundImage: `url(${
-              PROJECTS[(currentIndex - 1 + PROJECTS.length) % PROJECTS.length]
-                .thumbnail
-            })`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-          onClick={prevProject}
-        ></div>
-        <div className={centerStyle}>
+      <div {...bind()} className="touch-none">
+        <div className="flex justify-center items-center">
+          <div className="hidden md:block">
+            <button onClick={prevProject} aria-label="Previous Project">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </button>
+          </div>
           <div
-            className="absolute inset-0 rounded-lg opacity-25"
+            className={sideStyle}
             style={{
-              backgroundImage: `url(${PROJECTS[currentIndex].thumbnail})`,
+              backgroundImage: `url(${
+                PROJECTS[(currentIndex - 1 + PROJECTS.length) % PROJECTS.length]
+                  .thumbnail
+              })`,
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center",
             }}
+            onClick={prevProject}
           ></div>
-          <h2 className="relative z-10 text-2xl font-bold">
-            {PROJECTS[currentIndex].name}
-          </h2>
-          <p className="relative z-10 font-normal">
-            {PROJECTS[currentIndex].used.join(", ")}
-          </p>
-          <a
-            href={PROJECTS[currentIndex].github}
-            className="relative z-10 font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            GitHub
-          </a>
-          <a
-            href={PROJECTS[currentIndex].link}
-            className="relative z-10 font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            Link
-          </a>
-        </div>
-        <div
-          className={sideStyle}
-          style={{
-            backgroundImage: `url(${
-              PROJECTS[(currentIndex + 1) % PROJECTS.length].thumbnail
-            })`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-          onClick={nextProject}
-        ></div>
-        <div className="hidden md:block">
-          <button onClick={nextProject} aria-label="Next Project">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
+          <div className={centerStyle}>
+            <div
+              className="absolute inset-0 rounded-lg opacity-25"
+              style={{
+                backgroundImage: `url(${PROJECTS[currentIndex].thumbnail})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            <h2 className="relative z-10 text-2xl font-bold">
+              {PROJECTS[currentIndex].name}
+            </h2>
+            <p className="relative z-10 font-normal">
+              {PROJECTS[currentIndex].used.join(", ")}
+            </p>
+            <a
+              href={PROJECTS[currentIndex].github}
+              className="relative z-10 font-medium text-blue-600 dark:text-blue-500 hover:underline"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
-          </button>
+              GitHub
+            </a>
+            <a
+              href={PROJECTS[currentIndex].link}
+              className="relative z-10 font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Link
+            </a>
+          </div>
+          <div
+            className={sideStyle}
+            style={{
+              backgroundImage: `url(${
+                PROJECTS[(currentIndex + 1) % PROJECTS.length].thumbnail
+              })`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+            onClick={nextProject}
+          ></div>
+          <div className="hidden md:block">
+            <button onClick={nextProject} aria-label="Next Project">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        {/* Project Carousel Pointer*/}
+        <div>
+          <div className="flex justify-center mt-4">
+            {PROJECTS.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentIndex(index)}
+                className={`${
+                  index === currentIndex
+                    ? "bg-white -scale-x-150 w-4 translate-x-0 ease duration-300 "
+                    : "bg-gray-500"
+                } w-2 h-2 rounded-full mx-2 focus:outline-none`}
+              ></button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
